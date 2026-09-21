2a382191f128970774ec403ea8f99bfbd72606cc
Vanger47 <117186289+Vanger47@users.noreply.github.com>
2026-09-21 18:07:24 +0200

    jalon 1

--- sample-jalon1/client.c
+++ sample-jalon1/client.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 200112L // GEMINI
+
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <netinet/in.h>
@@ -6,8 +8,11 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <poll.h>
 
 #include "common.h"
+#define FDS_SIZE 2
+
 
 void die(ssize_t ret_value, const char * msg){
 	if (ret_value < 0){
@@ -15,39 +20,128 @@ void die(ssize_t ret_value, const char * msg){
 		exit(EXIT_FAILURE);
 	}
 }
+int write_on_socket(int sockfd, void * buf, int to_write )
+{
+    int written_bytes = 0;
+    int ret_value = 0;
+
+    while (written_bytes != to_write){
+    ret_value = write(sockfd, (char *)buf + written_bytes, to_write - written_bytes);
+    if (ret_value == 0){
+        printf("Disconnected\n");
+        exit(EXIT_FAILURE);
+        }
+    die(ret_value, "reading msg header");
+    written_bytes += ret_value;
+
+    }
+    return written_bytes;
+}
+
+
+
+int read_on_socket(int fd, void * ptr, int size) {
+
+    int read_bytes = 0;
+    int ret_value = 0;
+
+    while (read_bytes != size){
+    ret_value = read(fd, (char *)ptr + read_bytes, size - read_bytes);
+    if (ret_value == 0){
+        printf("Disconnected\n");
+        return 0;
+        }
+    die(ret_value, "reading msg header");
+    read_bytes += ret_value;
+
+
+    }
+    return read_bytes;
+}
+
+
 
 void echo_client(int sockfd) {
 	char buff[MSG_LEN];
 	int n;
+	struct pollfd fds[FDS_SIZE];
+	fds[0].fd = STDIN_FILENO;
+	fds[0].events = POLLIN;
+	fds[0].revents = 0;
+
+	fds[1].fd = sockfd;
+	fds[1].events = POLLIN;
+	fds[1].revents = 0;
+
 	while (1) {
-		// Cleaning memory
-		memset(buff, 0, MSG_LEN);
-		// Getting message from client
+
+		int nb_active_fd;
 		printf("Message: ");
-		n = 0;
-		while ((buff[n++] = getchar()) != '\n') {} // trailing '\n' will be sent
-		// Sending message (ECHO)
-		if (send(sockfd, buff, strlen(buff), 0) <= 0) {
-			break;
+		fflush(stdout); // pour reinitialiser le tampon
+		nb_active_fd = poll(fds, FDS_SIZE, -1);
+		die(nb_active_fd, "on polling");
+
+		if (fds[0].revents & POLLIN)
+		{
+			memset(buff, 0, MSG_LEN);
+			// Getting message from client
+			n = 0;
+			while ((buff[n++] = getchar()) != '\n' && n < MSG_LEN) {} // trailing '\n' will be sent
+			
+            if (strncmp(buff, "/quit\n", 6) == 0) {
+                int msg_size = strlen(buff);
+                write_on_socket(sockfd, &msg_size, sizeof(int));
+                write_on_socket(sockfd, buff, msg_size);
+                
+                printf("Fermeture de la connexion...\n");
+                break; // On sort de la boucle infinie
+            }
+
+			int msg_size = strlen(buff);
+			if (write_on_socket(sockfd, &msg_size, sizeof(int)) < 0)
+			{
+				break;
+			}
+			
+			if (write_on_socket(sockfd, buff, msg_size) < 0)
+			{
+				break;
+			}
+
+			printf("Message sent!\n");
+			fds[0].revents = 0;
 		}
-		printf("Message sent!\n");
-		// Cleaning memory
-		memset(buff, 0, MSG_LEN);
-		// Receiving message
-		if (recv(sockfd, buff, MSG_LEN, 0) <= 0) {
-			break;
-		}
-		printf("Received: %s", buff);
+		
+		if (fds[1].revents & POLLIN)
+		{
+			memset(buff, 0, MSG_LEN);
+			// Receiving message
+			int incoming_size = 0;
+			if (read_on_socket(sockfd, &incoming_size, sizeof(int)) < 0)
+			{
+				fds[1].revents = 0;
+				break;
+			}
+			if (incoming_size > MSG_LEN - 1) incoming_size = MSG_LEN - 1;
+			if (read_on_socket(sockfd, buff, incoming_size) < 0)
+			{
+				fds[1].revents = 0;
+				break;
+			}
+			buff[incoming_size] = '\0';
+			printf("\nReceived: %s", buff);
+
+		}		
 	}
 }
 
-int handle_connect() {
+int handle_connect(const char * server_name, const char * server_port) {
 	struct addrinfo hints, *result, *rp;
 	int sfd;
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
-	if (getaddrinfo(SERV_ADDR, SERV_PORT, &hints, &result) != 0) {
+	if (getaddrinfo(server_name, server_port, &hints, &result) != 0) {
 		perror("getaddrinfo()");
 		exit(EXIT_FAILURE);
 	}
@@ -69,9 +163,14 @@ int handle_connect() {
 	return sfd;
 }
 
-int main() {
+int main(int argc, char * argv[]) {
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage : %s <server_name> <server_port>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 	int sfd;
-	sfd = handle_connect();
+	sfd = handle_connect(argv[1], argv[2]);
 	echo_client(sfd);
 	close(sfd);
 	return EXIT_SUCCESS;
--- sample-jalon1/server.c
+++ sample-jalon1/server.c
@@ -8,35 +8,155 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <poll.h>
 
 #include "common.h"
+#define MAX_CLIENTS 10
+
+void die(ssize_t ret_value, const char * msg){
+	if (ret_value < 0){
+		perror(msg);
+		exit(EXIT_FAILURE);
+	}
+}
+int write_on_socket(int sockfd, void * buf, int to_write )
+{
+    int written_bytes = 0;
+    int ret_value = 0;
+
+    while (written_bytes != to_write){
+    ret_value = write(sockfd, (char *)buf + written_bytes, to_write - written_bytes);
+    if (ret_value == 0){
+        printf("Disconnected\n");
+        exit(EXIT_FAILURE);
+        }
+    die(ret_value, "reading msg header");
+    written_bytes += ret_value;
+
+    }
+    return written_bytes;
+}
+
+
+
+int read_on_socket(int fd, void * ptr, int size) {
+
+    int read_bytes = 0;
+    int ret_value = 0;
+
+    while (read_bytes != size){
+    ret_value = read(fd, (char *)ptr + read_bytes, size - read_bytes);
+    if (ret_value == 0){
+        printf("Disconnected\n");
+        return 0;
+        }
+    die(ret_value, "reading msg header");
+    read_bytes += ret_value;
+
+
+    }
+    return read_bytes;
+}
 
 void echo_server(int sockfd) {
 	char buff[MSG_LEN];
-	while (1) {
-		// Cleaning memory
-		memset(buff, 0, MSG_LEN);
-		// Receiving message
-		if (recv(sockfd, buff, MSG_LEN, 0) <= 0) {
-			break;
-		}
-		printf("Received: %s", buff);
-		// Sending message (ECHO)
-		if (send(sockfd, buff, strlen(buff), 0) <= 0) {
-			break;
-		}
-		printf("Message sent!\n");
+	struct pollfd fds[MAX_CLIENTS];
+
+	fds[0].fd = sockfd;
+	fds[0].events = POLLIN;
+	fds[0].revents = 0;
+
+	for(int i = 1; i < MAX_CLIENTS; i++)
+	{
+		fds[i].fd = -1;
+		fds[i].events = 0;
+		fds[i].revents = 0;
+	}
+	printf("Server listenning ...\n");
+
+	while (1) {;
+		printf("Waiting for activity...\n");
+		int nb_active_fd = poll(fds, MAX_CLIENTS, -1);
+		die(nb_active_fd, "on polling");
+
+		for (int i = 0; i < MAX_CLIENTS; i++)
+		{
+			if (i == 0 && (fds[0].revents & POLLIN))
+			{
+				struct sockaddr cli;
+				socklen_t len = sizeof(cli);
+				int client_fd = accept(sockfd, (struct sockaddr*)&cli, &len);
+
+				if (client_fd < 0)
+				{
+					perror("accept"); 
+					continue;
+				}
+
+				printf("New client %d\n", client_fd);
+
+				for (size_t j = 1; j < MAX_CLIENTS; j++) //on cherche une case libre
+				{
+					if (fds[j].fd == -1)
+					{
+						fds[j].fd = client_fd;
+						fds[j].events = POLLIN;
+						fds[j].revents = 0;
+						break;
+					}
+				}
+			}
+
+		else if (i > 0 && (fds[i].revents & POLLIN))
+		{
+			memset(buff, 0, MSG_LEN);
+			int incoming_size = 0;
+
+			buff[incoming_size] = '\0';
+            
+            // --- NOUVEAU BLOC REQ 1.7 ---
+            if (strncmp(buff, "/quit\n", 6) == 0) {
+                printf("Client %d a demandé la déconnexion (/quit).\n", fds[i].fd);
+                close(fds[i].fd);
+                fds[i].fd = -1; 
+                continue; 
+            }
+
+			if (read_on_socket(fds[i].fd, &incoming_size, sizeof(int)) < 0) {
+				fprintf(stderr, "Client disconnected fd = %d\n", fds[i].fd);
+				close(fds[i].fd);
+				fds[i].fd = -1;
+				continue;
+			}
+
+			if (incoming_size > MSG_LEN - 1) incoming_size = MSG_LEN - 1;
+
+			if (read_on_socket(fds[i].fd, buff, incoming_size) < 0) {
+				fprintf(stderr, "Client disconnected fd = %d\n", fds[i].fd);
+				close(fds[i].fd);
+				fds[i].fd = -1;
+				continue;
+			}
+			buff[incoming_size] = '\0';
+			printf("Received: %s", buff);
+
+			write_on_socket(fds[i].fd, &incoming_size, sizeof(int));
+			write_on_socket(fds[i].fd, buff, incoming_size);
+			printf("Message sent to %d!\n", fds[i].fd);
+			
+		}		
 	}
 }
+}
 
-int handle_bind() {
+int handle_bind(const char * server_port) {
 	struct addrinfo hints, *result, *rp;
 	int sfd;
 	memset(&hints, 0, sizeof(struct addrinfo)); // hints c'est une structure où on va mettre ce qu'on cherche 
 	hints.ai_family = AF_UNSPEC; //Ipv4 et IPv6
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE; // On écoute toutes les inferfaces réseaux disponibles
-	if (getaddrinfo(NULL, SERV_PORT, &hints, &result) != 0) {
+	if (getaddrinfo(NULL, server_port, &hints, &result) != 0) {
 		perror("getaddrinfo()");
 		exit(EXIT_FAILURE);
 	}
@@ -59,21 +179,20 @@ int handle_bind() {
 	return sfd;
 }
 
-int main() {
-	struct sockaddr cli;
-	int sfd, connfd;
-	socklen_t len;
-	sfd = handle_bind();
+int main(int argc, char * argv[]) {
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage : %s <server_port>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	
+	int sfd = handle_bind(argv[1]);
 	if ((listen(sfd, SOMAXCONN)) != 0) {  //SOMAXCONN = la plus grande file d'attente pour les clients
 		perror("listen()\n");
 		exit(EXIT_FAILURE);
 	}
-	len = sizeof(cli); // on prépare la taille max 
-	if ((connfd = accept(sfd, (struct sockaddr*) &cli, &len)) < 0) { // &len = 16 pour ipv4 et 28 pour ipv6
-		perror("accept()\n");
-		exit(EXIT_FAILURE);
-	}
-	echo_server(connfd);
+	
+	echo_server(sfd);
 	close(sfd);
 	return EXIT_SUCCESS;
 }
